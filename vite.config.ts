a710997373063932f4568baf09b3e6ad1c669e6a
aspart-am <florian.esparsa@gmail.com>
2025-04-18 16:30:19 +0000

    correction

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base:'/ACI-REACT/',
+  base: '/ACI-REACT/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
