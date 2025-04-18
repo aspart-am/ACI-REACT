f6fc65937a2d80e36dbfad772354dc7c8acf20f7
aspart-am <florian.esparsa@gmail.com>
2025-04-18 17:58:15 +0000

    Simplification de App.tsx pour le déploiement initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
-import Indicators from './pages/Indicators'
-import Associates from './pages/Associates'
-import Missions from './pages/Missions'
 import Layout from './components/Layout'
-import IndicatorDetail from './pages/IndicatorDetail'
-import AssociateDetail from './pages/AssociateDetail'
-import MissionDetail from './pages/MissionDetail'
 
 function App() {
   return (
@@ -15,12 +9,13 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/indicators" element={<Indicators />} />
-          <Route path="/indicators/:id" element={<IndicatorDetail />} />
-          <Route path="/associates" element={<Associates />} />
-          <Route path="/associates/:id" element={<AssociateDetail />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/missions/:id" element={<MissionDetail />} />
+          {/* Ces routes seront implémentées ultérieurement */}
+          {/* <Route path="/indicators" element={<Indicators />} /> */}
+          {/* <Route path="/indicators/:id" element={<IndicatorDetail />} /> */}
+          {/* <Route path="/associates" element={<Associates />} /> */}
+          {/* <Route path="/associates/:id" element={<AssociateDetail />} /> */}
+          {/* <Route path="/missions" element={<Missions />} /> */}
+          {/* <Route path="/missions/:id" element={<MissionDetail />} /> */}
         </Routes>
       </Layout>
     </div>
